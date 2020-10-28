63cdce23abf7640c07bba8791ef9542d62d7775d
Mitko <gurbanskimitko@gmail.com>
2020-10-28 12:29:35 -0700

    Project Init

---
+++ src/test/java/com/finki/mojmentor/MojMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.mojmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
